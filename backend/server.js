5bbc309b40fa584f14c563bdfa14a536de8fdd35
ghulam mustafa <gm0850296@gmailcom>
2026-07-02 23:41:53 +0500

    fix images and footer

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,6 @@ app.get("/", (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,"0.0.0.0" ,() => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
