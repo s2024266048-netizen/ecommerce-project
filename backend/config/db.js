90d51a28edf0572dc9c8f557444649e1a47fbc39
ghulam mustafa <gm0850296@gmailcom>
2026-07-03 14:17:43 +0500

    debug env vars

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
